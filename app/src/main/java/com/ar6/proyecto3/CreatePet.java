da5c322cf6d8fde4a6027e96b567b849aedbbebe
androd6 <73150533+androd6@users.noreply.github.com>
2020-11-26 14:18:39 -0500

    2020/111/26 Version 1

--- app/src/main/java/com/ar6/proyecto3/CreatePet.java
+++ app/src/main/java/com/ar6/proyecto3/CreatePet.java
@@ -1,4 +1,19 @@
 package com.ar6.proyecto3;
 
+import java.util.ArrayList;
+
 public class CreatePet {
+
+    public CreatePet() {
+
+        try {
+            ArrayList<Pet> varPets = new ArrayList();
+
+            varPets.add(new Pet());
+        }
+        catch (Exception ex){
+
+        }
+    }
+
 }
